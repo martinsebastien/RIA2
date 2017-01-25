d0847bfed662c50dc808bf49868bd7f9f3d92e77
MARTIN Sebastien <Sebastien.MARTIN@cpnv.ch>
2017-01-25 17:47:37 +0100

    add orientation when moving

--- src/prefabs/Unit.js
+++ src/prefabs/Unit.js
@@ -12,8 +12,12 @@ export default class Unit extends Prefab {
     load_stats(classes_data) {
         this.stats = Object.create(classes_data[this.unit_class]);
 
-        this.healthbar = this.game_state.game.add.sprite(this.x, this.y - (this.height / 2 + 5), "healthbar_image");
+        this.healthbar = this.game_state.game.add.sprite(this.x, this.y - (this.height / 2 + 10), "healthbar_image");
         this.healthbar.scale.setTo(this.stats.health, 1);
+        this.moveDuration = Phaser.Timer.SECOND * 0.3;
+        this.currentTweens = [];
+        this.moving = false;
+        this.tweenInProgress = false;
 
         this.attacked_tween = this.game_state.game.add.tween(this);
         this.attacked_tween.to({ tint: 0xFF0000 }, 200);
@@ -23,24 +27,65 @@ export default class Unit extends Prefab {
     }
 
     move_to(position) {
+        this.resetCurrentTweens();
         this.game_state.pathfinding.find_path(this.position, position, this.follow_path, this);
     }
 
     follow_path(path) {
-        let next_position, moving_tween, healthbar_moving_tween;
+        let next_position, tween, healthbar_moving_tween, index;
+        index = 0;
 
-        moving_tween = this.game_state.game.tweens.create(this);
         healthbar_moving_tween = this.game_state.game.tweens.create(this.healthbar);
-
-        path.forEach(function (position) {
-            moving_tween.to({ x: position.x, y: position.y }, Phaser.Timer.SECOND * 0.3);
-            healthbar_moving_tween.to({ x: position.x, y: position.y - this.height }, Phaser.Timer.SECOND * 0.3);
-            this.faceNextTile(moving_tween);
-        }, this);
-
-        moving_tween.start();
+        path.forEach(position => {
+            this.currentTweens.push(this.game.add.tween(this.position).to({ x: position.x, y: position.y }, this.moveDuration));
+            healthbar_moving_tween.to({ x: position.x, y: position.y - (this.height / 2 + 10) }, this.moveDuration);
+            index++;
+        });
+        this.moveInPath();
         healthbar_moving_tween.start();
     }
+
+    moveInPath() {
+        if (this.currentTweens.length === 0) {
+            return;
+        }
+        let index = 1, me = this;
+        this.moving = true;
+        moveToNext(this.currentTweens[index]);
+        function moveToNext(tween) {
+            index++;
+            let nextTween = me.currentTweens[index];
+            if (nextTween != null) {
+                tween.onComplete.add(function () {
+                    me.tweenInProgress = false;
+                    moveToNext(nextTween);
+                });
+            } else {
+                // if i am the last tween
+                tween.onComplete.add(function () {
+                    me.onStopMovement();
+                });
+            }
+            tween.start();
+            me.faceNextTile(tween);
+            me.tweenInProgress = true;
+        }
+    }
+
+    onStopMovement() {
+        this.resetCurrentTweens();
+    }
+
+    resetCurrentTweens() {
+        var me = this;
+        this.currentTweens.map(function (tween) {
+            me.game.tweens.remove(tween);
+        });
+        this.currentTweens = [];
+        this.moving = false;
+        this.stopAnimation();
+    }
+
 
     calculate_act_turn(current_turn) {
         this.act_turn = current_turn + Math.ceil(100 / this.stats.speed);
